6a711333841fc0f94189a5b0d96add9b13ed4d15
QamaDev <qamar.amassi@coktilat.com>
2021-02-02 18:44:05 +0200

    order trace status design

--- balbid/Controller/Order/UI/ProductTraceViewController.swift
+++ balbid/Controller/Order/UI/ProductTraceViewController.swift
@@ -8,15 +8,25 @@
 import UIKit
 
 class ProductTraceViewController: BaseViewController {
+    
+    @IBOutlet weak var binCodeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupNavbar()
+        setupPinCodeLabelText()
     }
     
     private func setupNavbar(){
         self.title = "#9254112114455141"
+    }
+    
+    private func setupPinCodeLabelText(){
+        let mainText = (binCodeLabel.text!.components(separatedBy: ":")[0] + ":")
+        .convertToMutableAttributedString(with: UIFont.regular.withSize(12), and: #colorLiteral(red: 0.1647058824, green: 0.1647058824, blue: 0.1647058824, alpha: 1))
+        let secondaryText =  binCodeLabel.text!.components(separatedBy: ":")[1].convertToAttributedString(with: UIFont.bold.withSize(12), and: #colorLiteral(red: 0.8039215686, green: 0.1921568627, blue: 0.1333333333, alpha: 1))
+        mainText.append(secondaryText)
+        binCodeLabel.attributedText = mainText
     }
     
 
