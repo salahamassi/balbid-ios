e5c07969207d5620bf4e3d893057b3d7263f2699
QamaDev <qamar.amassi@coktilat.com>
2021-02-02 21:16:41 +0200

    detect user location

---
+++ balbid/Controller/Order/UI/OrderTraceMapViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderTraceMapViewController.swift
+//  balbid
+//
+//  Created by Memo Amassi on 02/02/2021.
+//
+
+import UIKit
+
+class OrderTraceMapViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
